35d8232ddb738efc32cb5b29100aea8d43e22753
rajiniram <rmprasad382@gmail.com>
2026-03-01 14:08:22 +0530

    footer spacing issue fixed

--- app/components/SocialSection.tsx
+++ app/components/SocialSection.tsx
@@ -3,12 +3,11 @@ import FollowMe from "./FollowMe";
 
 export default function SocialSection({ title }: { title: string }) {
     return (
-        <div className="mt-24 text-center">
+        <div className="mt-16 text-center">
 
             <SocialShare title={title} />
-
             {/* Elegant separation */}
-            <div className="w-12 h-[1px] bg-gray-100 mx-auto my-20"></div>
+            <div className="w-12 h-[1px] bg-gray-200 mx-auto my-12"></div>
 
             <FollowMe />
 
