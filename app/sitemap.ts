945c44dae27f4905e823788caa3ae1b44d86850d
rajiniram <rmprasad382@gmail.com>
2026-03-15 23:31:34 +0530

    update sitemap with all pages

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,31 @@
-﻿export default function sitemap() {
+﻿import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = "https://thenomadicstory.com";
+
     return [
         {
-            url: "https://thenomadicstory.com",
+            url: `${baseUrl}`,
             lastModified: new Date(),
         },
         {
-            url: "https://thenomadicstory.com/hong-kong",
+            url: `${baseUrl}/hong-kong`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/about-me`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/gallery`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/blog`,
+            lastModified: new Date(),
+        },
+        {
+            url: `${baseUrl}/contact`,
             lastModified: new Date(),
         },
     ];
